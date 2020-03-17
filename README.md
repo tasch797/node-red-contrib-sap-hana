b31fd0a729f3dc76eb9ac3d61ec6812c9efbc4c7
radu103 <radu103@hotmail.com>
2020-03-17 12:55:15 +0200

    first draft version

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# node-red-contrib-sap-hana
-Node to execute commands &amp; query for SAP HANA database
+node-red-contrib-sap-hana
+=========================
+[Node-RED](http://nodered.org) nodes to work with a SAP HANA database
+
+Install
+-------
+Install from [npm](http://npmjs.org)
+```
+npm install node-red-contrib-sap-hana
+npm install -g @sap/hana-client
+cd $HOME/.node-red/node-modules
+npm link @sap/hana-client
+```
+
+Edit `/etc/hosts` for default hxehost replacing X.Y or whole IP address
+```
+192.168.X.Y hxehost
+```
+
+Usage
+-----
+This package contains one node to run a sql command, call a procedure or a select statement for SAP HANA database using the office @sap/hana-client Node.JS driver.
+
+
+Query node usage:
+-----------------
+
+You will need to fill in the following fields:
+
+-- Database host name and port.
+
+-- User name to access the database.
+
+-- Password for the user name above.
+
+-- Schema name.
+
+Node usage:
+------------------
+
+The returned data will be stored in msg.payload and it will contains the @sap/hana-client response.  
+
+To run a command  or a query input to saphana node the folowing message :
+```
+msg.topic = "SQL"; // or lower case 'sql'
+msg.payload = "SELECT * FROM DUMMY";
+return msg;
+```
+
+Currently, only SAP HANA Express has been tested.
+
+Please let me know if you have any issue.
+
+Authors
+-------
+* Radu Simen - [radu103@hotmail.com](mailto:radu103@hotmail.com)
---
+++ saphana.js
@@ -0,0 +1,96 @@
+module.exports = function(RED) {
+    
+	"use strict";
+    
+    var hanaClient = require('@sap/hana-client');
+	
+	function configNode(n) {
+        
+		//console.dir(n);
+		
+		RED.nodes.createNode(this, n);
+        
+		this.warn("Config reg : " + JSON.stringify(n));
+		
+		this.host = n.host;
+        this.port = parseInt(n.port);
+        this.user = n.user;
+		this.password = n.password;
+		this.dbname = n.db;
+    }
+	
+    RED.nodes.registerType("saphanaconfig", configNode);
+	
+    function sqlQueryIn(n) {
+		
+		//console.dir(n);
+		
+        RED.nodes.createNode(this, n);
+        
+		this.mydbConf = n.mydb;
+       
+        this.hanaConfig = RED.nodes.getNode(this.mydbConf);
+		
+		var node = this;		
+		
+		node.on("input", function(msg, send, done) {
+			
+			node.warn("Topic : " + msg.topic);
+			node.warn("Payload : " + msg.payload);
+			
+			if (msg.topic === 'SQL' || msg.topic === 'sql' ){
+				
+				var conn = hanaClient.createConnection();
+			
+				node.conn_params = {
+					host : node.hanaConfig.host,
+					port : node.hanaConfig.port,
+					user : node.hanaConfig.user,
+					password : node.hanaConfig.password
+				};
+				
+				node.warn("Connection Params : " + JSON.stringify(node.conn_params));
+				
+				conn.connect(node.conn_params, function(err, result) {
+					
+					node.connection = conn;
+					
+					if(err) {
+						node.warn(err);
+						
+						node.connected = false;
+						
+						node.warn("Connection failed for host " + node.hanaConfig.host + " with user " + node.hanaConfig.user);
+					}
+					else
+					{
+						conn.exec(msg.payload, [], function (err2, result2) {
+							
+							if (err2) {
+								node.warn(err2);
+							}
+							
+							node.connected = true;
+							
+							send(result2);
+							done();
+							
+							conn.disconnect();								
+						});
+					}
+
+					send({});
+					done();
+				});
+		
+			}
+			else {
+				if (typeof msg.topic !== 'string') { 
+					node.error("msg.topic : the query is not defined as a string"); 
+				}
+			}
+		});
+    }
+	
+    RED.nodes.registerType("saphana", sqlQueryIn);
+}
