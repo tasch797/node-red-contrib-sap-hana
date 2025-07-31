68f6299ba93d53ea3689b42c5a72295334b0cae6
tasch797 <83837605+tasch797@users.noreply.github.com>
2025-07-31 07:53:50 -0400

    Update sap_hana_config.js

--- sap_hana_config.js
+++ sap_hana_config.js
@@ -1,20 +1,21 @@
 module.exports = function(RED) {
-    
-	"use strict";
-	
-	function configNode(n) {
-        
-		//console.dir(n);
-		
-		RED.nodes.createNode(this, n);
-        
-		this.warn("Config reg : " + JSON.stringify(n));
-		
-		this.host = n.host;
+    "use strict";
+
+    function configNode(n) {
+        // Register the node with Node-RED
+        RED.nodes.createNode(this, n);
+
+        // ⚠️ Removed insecure logging of credentials
+        // If needed, you can enable debugging below without exposing sensitive data:
+        // this.debug("Initializing SAP HANA config with host=" + n.host + ", port=" + n.port);
+
+        // Assign configuration parameters to the node
+        this.host = n.host;
         this.port = parseInt(n.port);
         this.user = n.user;
-		this.password = n.password;
+        this.password = n.password;
     }
-	
+
+    // Register the node type
     RED.nodes.registerType("sap_hana_config", configNode);
 }
